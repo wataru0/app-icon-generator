7a1169af1e0f53717bcdddf680476e57cb1b8a02
Wataru Okamoto <okamoto.kawalab@gmail.com>
2021-09-11 23:40:44 +0900

    canvasでリサイズしローカルに保存できるようように変更

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import "bulma/css/bulma.css";
 
 function Header() {
@@ -10,62 +10,149 @@ function Header() {
             <h1 className="title">App Icon Generator</h1>
           </div>
         </div>
-
       </div>
     </header>
   );
 }
 function Image(props) {
+  if (props.fileUrl === null) {
+    return <div />;
+  }
   return (
     <div>
-      <figure className="image is-square">
+      <figure className="image">
         <img
           id="preview"
           // src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" 
           // 親コンポーネントから渡されたプロパティはpropsを使ってアクセスできる！！
           src={props.fileUrl}
-          alt="" />
+          alt=" " />
       </figure>
     </div>
   );
 }
 
-// const previewImage = (obj) => {
-//   let fileRoader = new FileReader();
-//   fileRoader.onload = (() => {
-//     // htmlの書き方
-//     // document.getElementById('preview').src = fileRoader.result;
-//   });
-//   fileRoader.readAsDataURL(obj.files[0]);
-// }
+function DlLink(props) {
+  const [boxValue, setBoxValue] = useState(['iOS']);
+  if (props.data === null || props.loaded === false || props.flag === false) {
+    return <div />;
+  }
+  return (
+    <div>
+      <div>
+        <label>
+          <input type="checkbox" name="iOS" value="1" /> iOS and macOS
+        </label>
+        <div />
+        <label>
+          <input type="checkbox" name="Android" value="2" /> Android
+        </label>
+      </div>
+      <a className="button is-fullwidth is-primary is-outlined" href={props.data} download="sample.png">download</a>
+    </div>
+  );
+}
 
 function Main() {
-  const [fileUrl, setFileUrl] = useState("https://images.dog.ceo/breeds/shiba/shiba-8.jpg");
+  const [fileUrl, setFileUrl] = useState(null);
+  const [resizeData, setResizeData] = useState(null);
+  const [flag, setFlag] = useState(false);
+
+  // Javascriptで画像処理を行うためには，読み込んだ画像をCanvasに渡してやる必要がある
+  const [canvas, setCanvas] = useState(null);
+  const [context, setContext] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
   const previewImage = (event) => {
     const imageFile = event.target.files.item(0);
     const imageUrl = URL.createObjectURL(imageFile);
-    setFileUrl(imageUrl)
+    setFileUrl(imageUrl);
+    setFlag(false);
   }
+
+  useEffect(() => {
+    const cv = document.createElement('canvas');
+    // cv.width = 1024;
+    // cv.height = 1024;
+    const ctx = cv.getContext('2d');
+    setCanvas(cv);
+    setContext(ctx);
+  }, []);
+
+  useEffect(() => {
+    if (context !== null) {
+      const img = document.createElement('img');
+      img.src = fileUrl;
+      img.onload = () => {
+        // 読み込み完了時に走る
+        // drawImageの第一引数はimageオブジェクト
+        // context.drawImage(img, 0, 0);
+        let dstWidth = 400;
+        let dstHeight = 400;
+        canvas.width = dstWidth;
+        canvas.height = dstHeight;
+        context.drawImage(img, 0, 0, 1024, 1024, 0, 0, dstWidth, dstHeight);
+        setLoaded(true);
+      }
+    }
+  }, [context, fileUrl]);
+
+  // useEffect(() => {
+  //   if (loaded) {
+  //     console.log(canvas);
+  //   }
+  // }, [loaded, canvas]);
+
+  const resizeImage = (canvas) => {
+    if (loaded) {
+      setResizeData(canvas.toDataURL('image/png'))
+    }
+    setFlag(true);
+  }
+
   return (
     <div className="container">
-      <div className="columns is-vcentered">
+      <br />
+      <div className="columns is-centered">
         <div className="column">
           <form>
-            <div className="box">
-              <div className="field">
+            <div className="file">
+              <label className="file-label">
                 <input id="inputImage" type="file" name="img"
-                  accept="image/*" onChange={previewImage}></input>
-              </div>
-              <Image fileUrl={fileUrl}/>
+                  accept="image/*" onChange={previewImage} className="file-input"
+                ></input>
+                <span className="file-cta">
+                  <span className="file-icon">
+                    <i className="fas fa-upload"></i>
+                  </span>
+                  <span className="file-label">
+                    Choose a file…
+                  </span>
+                </span>
+                {/* <span class="file-name">
+                  ...
+                </span> */}
+              </label>
             </div>
+            <Image fileUrl={fileUrl} />
           </form>
         </div>
         <div className="column">
           <div className="field is-centered">
-            <button className="button is-rounded is-primary" id="generateButton" type="submit">generate</button>
+            <button
+              className="button is-rounded is-primary is-fullwidth"
+              id="generateButton"
+              type="submit"
+              onClick={() => { resizeImage(canvas) }}>
+              {/* onClick={() => console.log(1)}> */}
+              Generate
+            </button>
           </div>
+          {/* <Canvas fileUrl={fileUrl} /> */}
+          <DlLink data={resizeData} loaded={loaded} flag={flag} />
         </div>
       </div>
+      <br />
     </div>
   );
 }
@@ -75,10 +162,13 @@ function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>Made with love in Chiba, Japan</p>
+        <a href="https://wataru0.github.io/portfolio/AppIconGenerator/PrivacyPolicy/">Privacy Policy</a>
+        <p> By wataru0</p>
       </div>
     </footer>
   );
 }
+
 function App() {
   return (
     <div>
